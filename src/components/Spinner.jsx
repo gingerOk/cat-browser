834a578f44b971787fd68e0961eb28c5a83ef9e2
gingerOk <oksanka.tarasyuk@gmail.com>
2021-11-15 16:52:07 +0200

    add cat spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,29 @@
 import { Spinner } from "react-bootstrap";
-
+import './Spinner.scss';
 const SpinnerCircle = () => {
-  return <Spinner animation="border" size="lg" variant="dark" />;
+  return (
+    <div className="loading-wrapper">
+    <div className="loading-cat">
+          <div className="cat-body" />
+          <div className="cat-animation-mask" />
+          <div className="cat-head">
+            <div className="cat-face" />
+            <div className="cat-ear" />
+            <div className="cat-hand" />
+            <div className="cat-eye" />
+            <div className="cat-eye-light" />
+            <div className="cat-mouth" />
+            <div className="cat-beard left" />
+            <div className="cat-beard right" />
+          </div>
+          <div className="cat-foot">
+          <div className="cat-belly" />
+          <div className="cat-leg" />
+          <div className="cat-tail" />
+          </div>
+    </div>  
+    </div>
+ )
 };
 
 export default SpinnerCircle;
