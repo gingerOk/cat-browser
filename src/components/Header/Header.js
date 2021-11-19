32bb439f27de7be15e585829063cbdf41af8c4d2
gingerOk <oksanka.tarasyuk@gmail.com>
2021-11-19 22:21:47 +0200

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { ROUTE_HOME } from '../../constants/constants';
 import './Header.scss';
 
 const Header = () => {
+  const history = useHistory();
+  const navigateToHome = () => history.push(ROUTE_HOME);
   return (
     <div className="header-wrapper">
       <div className="header-container">
-        <h1>Cat Browser</h1>
-        <div className="header-animation">
-          {new Array(6).fill('img').map((item, index) => (
-            <img src="/images/paw-print.svg" width="20" height="20" key={index} />
-          ))}
-        </div>
+        <h1 onClick={navigateToHome}>Dogs & Cats</h1>
       </div>
     </div>
   );
